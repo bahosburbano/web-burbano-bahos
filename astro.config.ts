a08ffb97074df7e4c3e237a619ad0fe288643919
bahosburbano <fauerbahos@gmail.com>
2026-01-02 05:22:27 -0500

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
-  // Configuración mínima para Bahos & Burbano
   build: {
     format: 'file'
   }
